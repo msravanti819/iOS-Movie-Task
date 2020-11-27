0e8b6353122dc580f1467af43d1751c529438583
Sravanti <sravi.sravanti73@gmail.com>
2020-11-27 16:19:00 +0530

    Initial Commit

---
+++ iOS-Movie_App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS-Movie_App
+//
+//  Created by Sravanti on 27/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
